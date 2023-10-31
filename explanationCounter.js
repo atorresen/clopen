7b7cf2927db1cc0d6ac46bea5bae4e680fd2f34d
AnneMarie Torresen <torresen@umich.edu>
2023-10-30 21:26:14 -0400

    Reinstate clopen page (permanent) and revealExplanation (temporary)

--- explanationCounter.js
+++ explanationCounter.js
@@ -1,15 +1,15 @@
 var pIndex = 0;
 
 function revealExplanation(pLen, begin, counter) {
-  // if (counter > begin && pIndex <= pLen) {
-  //   var x = document.getElementById("p" + pIndex);
-  //   x.style.display = "block";
-  //   if (pIndex == pLen) {
-  //     var div = document.getElementById("explain");
-  //     var end = document.createElement('p');
-  //     end.innerHTML = "𖣯";
-  //     div.appendChild(end);
-  //   }
-  //   pIndex++;
-  // }
+  if (counter > begin && pIndex <= pLen) {
+    var x = document.getElementById("p" + pIndex);
+    x.style.display = "block";
+    if (pIndex == pLen) {
+      var div = document.getElementById("explain");
+      var end = document.createElement('p');
+      end.innerHTML = "𖣯";
+      div.appendChild(end);
+    }
+    pIndex++;
+  }
 };
